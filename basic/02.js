eda6e55e519ab08b93429c6c2718d3a4806664a0
cprince9 <155155054+cprince9@users.noreply.github.com>
2026-03-13 20:10:31 +0000

    DataTypes

---
+++ basic/02.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all js code as newer version
+
+console.log("hey");
+// alert("Prince"); //we are using node, not browser
+
+// datatype
+// number = - (2power53 - 1) to (2power53 - 1)
+// BigInt < (2power53 - 1) esse bade value BigInt mei
+// null = standalone value
+// undefined = currently value is not initailise
+// symbol = unique
+
+// null is an Object
+console.log(typeof null);
+
+console.log(typeof undefined);
+
